8d0c87420511b77e17d5842df25cef3ae066a701
Ayush Dhanawade <717ayush@gmail.com>
2025-11-28 13:47:03 +0000

    can we put small line between therse all joints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,21 @@ import { ContactSection } from "@/components/contact-section";
 import { HeroSection } from "@/components/hero-section";
 import { ProjectsSection } from "@/components/projects-section";
 import { SkillsSection } from "@/components/skills-section";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Separator />
       <AboutSection />
+      <Separator />
       <SkillsSection />
+      <Separator />
       <ProjectsSection />
+      <Separator />
       <CoCurricularSection />
+      <Separator />
       <ContactSection />
     </>
   );
